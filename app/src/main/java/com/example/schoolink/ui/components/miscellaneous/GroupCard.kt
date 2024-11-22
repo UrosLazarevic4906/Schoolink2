3177df2143fa5440456a73e5bc927dec8ad8ba67
Uros <uros.lazarevic.4906@metropolitan.ac.rs>
2024-11-22 17:45:33 +0100

    Created Group Cards

--- app/src/main/java/com/example/schoolink/ui/components/miscellaneous/GroupCard.kt
+++ app/src/main/java/com/example/schoolink/ui/components/miscellaneous/GroupCard.kt
@@ -7,18 +7,16 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -28,20 +26,21 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil3.compose.rememberAsyncImagePainter
-import com.example.schoolink.domain.models.StudentModel
 import com.example.schoolink.R
-import com.example.schoolink.domain.models.Gender
+import com.example.schoolink.domain.models.GroupModel
+import com.example.schoolink.domain.models.GroupType
 import com.example.schoolink.ui.theme.*
 
 @Composable
-fun StudentCardBox(
-    student: StudentModel,
-    isChecked: Boolean = false,
+fun GroupCard(
+    group: GroupModel,
     showTopLine: Boolean = false,
-    onCheckChange: (Boolean) -> Unit
+    onClick: () -> Unit
 ) {
     Surface(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onClick() },
         color = White,
     ) {
         Column {
@@ -59,15 +58,15 @@ fun StudentCardBox(
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier.padding(16.dp)
         ) {
-            val profilePicture = if (!student.profilePicturePath.isNullOrEmpty()) {
-                rememberAsyncImagePainter(model = student.profilePicturePath)
+            val groupImage = if (!group.groupPicturePath.isNullOrEmpty()) {
+                rememberAsyncImagePainter(model = group.groupPicturePath)
             } else {
-                painterResource(id = R.drawable.ic_user)
+                painterResource(id = R.drawable.ic_group)
             }
 
             Image(
-                painter = profilePicture,
-                contentDescription = "${student.studentCode} profile picture",
+                painter = groupImage,
+                contentDescription = "${group.groupName} group picture",
                 modifier = Modifier
                     .size(52.dp)
                     .clip(CircleShape),
@@ -80,22 +79,24 @@ fun StudentCardBox(
                 modifier = Modifier.weight(1f)
             ) {
                 Text(
-                    text = "${student.firstName} ${student.lastName}",
+                    text = group.groupName,
                     color = Black,
                     style = MaterialTheme.typography.bodyLarge,
                     fontWeight = FontWeight.Bold
                 )
                 Text(
-                    text = student.studentCode,
+                    text = group.groupType!!.name,
                     color = Ash,
                     style = MaterialTheme.typography.bodySmall,
                     fontWeight = FontWeight.Medium
                 )
             }
 
-            Checkbox(
-                checked = isChecked,
-                onCheckedChange = onCheckChange
+            Icon(
+                painter = painterResource(R.drawable.ic_chevron_right),
+                contentDescription = "Trailing icon",
+                tint = Black,
+                modifier = Modifier.size(30.dp)
             )
         }
     }
@@ -104,23 +105,26 @@ fun StudentCardBox(
 
 @Preview
 @Composable
-private fun StudentCardBoxPreview() {
+private fun GroupCardPreview() {
 
-    val dummyStudent = StudentModel(
-        id = 0,
-        email = "asd",
-        firstName = "Uros",
-        lastName = "Lazarevic",
-        dateOfBirth = "14/12/1999",
-        description = "description",
-        gender = Gender.MALE,
-        profilePicturePath = null,
-        studentCode = "A2D53AC"
+//    val dummyStudent = StudentModel(
+//        id = 0,
+//        email = "asd",
+//        firstName = "Uros",
+//        lastName = "Lazarevic",
+//        dateOfBirth = "14/12/1999",
+//        description = "description",
+//        gender = Gender.MALE,
+//        profilePicturePath = null,
+//        studentCode = "A2D53AC"
+//    )
+    val dummyGroup = GroupModel(
+        groupId = 0,
+        groupName = "Name of group",
+        groupType = GroupType.ADVANCED,
     )
-
-    StudentCardBox(
-        student = dummyStudent,
-        isChecked = true,
-        onCheckChange = {},
+    GroupCard(
+        dummyGroup,
+        onClick = {}
     )
 }
