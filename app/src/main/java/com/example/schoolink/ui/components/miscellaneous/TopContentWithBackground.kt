d2e741ea50e70aa5deca6dc14872fe872a032f69
Uros <uros.lazarevic.4906@metropolitan.ac.rs>
2024-11-26 18:08:59 +0100

    Changed top content

--- app/src/main/java/com/example/schoolink/ui/components/miscellaneous/TopContentWithBackground.kt
+++ app/src/main/java/com/example/schoolink/ui/components/miscellaneous/TopContentWithBackground.kt
@@ -2,26 +2,32 @@ package com.example.schoolink.ui.components.miscellaneous
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import coil3.compose.rememberAsyncImagePainter
 import com.example.schoolink.ui.screens.main.Screen
 import com.example.schoolink.R
+import com.example.schoolink.domain.models.ProfessorModel
 import com.example.schoolink.ui.theme.White
 
 @Composable
 fun TopContentWithBackground(
-    screen: Screen
+    screen: Screen,
+    professor: ProfessorModel?
 ) {
 
     Image(
@@ -31,20 +37,37 @@ fun TopContentWithBackground(
         modifier = Modifier.fillMaxWidth()
     )
 
-    Column (
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
-    ){
-        Image(
-            painter = painterResource(R.drawable.ic_user),
-            contentDescription = "Profile picture",
-            modifier = Modifier.size(80.dp)
-        )
+            .padding(horizontal = 28.dp, vertical = 32.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(80.dp)
+        ) {
+            if (professor?.profilePicturePath != null) {
+                Image(
+                    painter = rememberAsyncImagePainter(professor.profilePicturePath),
+                    contentDescription = "Profile picture",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxSize(),
+                )
+            } else {
+                Image(
+                    painter = painterResource(R.drawable.ic_user),
+                    contentDescription = "Profile picture",
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier.fillMaxSize(),
+                )
+            }
+        }
 
         Text(
             text = screen.title,
-            style = MaterialTheme.typography.titleLarge,
+            style = MaterialTheme.typography.displaySmall,
             color = White
         )
 
